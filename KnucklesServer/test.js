7a64f2f2a985a68a53e056a8a57fd9ed2bf18e5f
Pavel Shub <pavel@citymaps.com>
2015-04-11 22:51:18 -0400

    pwm test?

--- KnucklesServer/test.js
+++ KnucklesServer/test.js
@@ -1,18 +1,28 @@
 var mraa = require('mraa');
 
-var pinNumber = 13;
+var pinNumber = 3;
 var analogPin = 0;
 
-var digitalPin = new mraa.Gpio(pinNumber);
-var analogPin = new mraa.Aio(analogPin);
+//var digitalPin = new mraa.Gpio(pinNumber);
+//var analogPin = new mraa.Aio(analogPin);
 
-digitalPin.dir(mraa.DIR_OUT); // vs DIR_IN
+var x = mraa.Pwm(pinNumber);
+x.period_us(700);
+x.enable(True);
+var value = 0.0;
+//digitalPin.dir(mraa.DIR_OUT); // vs DIR_IN
 
 var state = 0;
 
 setInterval(function(){
-        console.log(analogPin.read());
+        //console.log(analogPin.read());
         //state = 1 - state;
         //digitalPin.write(state);
-}, 1000);
+	x.write(value);
+	value = value + 0.01;
+	if(value >= 1.0){
+		value = 0.0;	
+	}
+
+}, 50);
 
