ff77104a7b26be1dbb379aad8e10dffef0fee5d4
Pavel Shub <pavel@citymaps.com>
2015-04-12 03:08:53 -0400

    getting closer?

--- KnucklesServer/server.js
+++ KnucklesServer/server.js
@@ -10,6 +10,10 @@ var mraa = require('mraa');
 var OUT_PIN = 3;
 var out = new mraa.Gpio(OUT_PIN);
 out.dir(mraa.DIR_OUT);
+var is_on = false;
+out.write(0);
+
+var settings = {"min_temp":23.0 ,"manual":true}
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -22,6 +26,62 @@ for(var i = 0; i < SENSOR_COUNT; i++){
 	sensors[i] = new mraa.Aio(i);
 }
 
+function getState() {
+  return {
+    "sensors":readings,
+    "temps":temps,
+    "is_heating": is_on
+  };
+}
+
+function manageTemp(t) {
+	if(!settings.manual){
+		if(t <= settings.min_temp && !is_on){
+			is_on = true;
+			out.write(1);
+			console.log("(auto) turned on");
+		} else if(t => settings.min_temp && is_on){
+			is_on = false;
+			out.write(0);
+			console.log("(auto) turned off");	
+		}
+	}
+}
+
+app.get('/heat', function (req, res) {
+  var data = getState();
+  res.send(data);
+});
+
+app.post('/heat', function (req, res) {
+  var body = req.body;
+  var on = 0;
+
+  if('settings' in body){
+    if('min_temp' in body.settings){
+      settings.min_temp = body.settings.min_temp;
+    }
+    if('manual' in body.settings){
+      settings.manual = body.settings.manual;
+    }
+  }
+
+  if(settings.manual){
+    if(body.on){
+      is_on = true;
+      on = 1;
+    } else {
+      is_on = false;
+      on = 0;
+    }
+  }
+
+  out.write(on);
+  var resp = {"request": body, "state": getState(), "settings": settings};
+  console.log(resp);
+  res.send(resp);
+});
+
 var readings = [];
 var temps = [];
 setInterval(function(){
@@ -30,28 +90,6 @@ setInterval(function(){
 		temps[i] = f.btot(readings[i]).t_c;
 	}
 }, 100);
-
-app.get('/heat', function (req, res) {
-
-  var data = {
-    "sensors":readings,
-    "temps":temps
-  };
-
-  res.send(data);
-});
-
-app.post('/heat', function (req, res) {
-  var body = req.body;
-  var on = 0;
-  if(body.on == "1"){
-    on = 1;
-  }
-  out.write(on);
-  var resp = {"results":0, "request": body, "on":on};
-  console.log(resp);
-  res.send(resp);
-});
 
 var server = app.listen(8080, function () {
 
